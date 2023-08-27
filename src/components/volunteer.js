0c393ec4111e69c23da008044f35275e79ce7152
Aditya <adityasahas2025@gmail.com>
2023-08-27 11:36:14 -0700

    animations

--- src/components/volunteer.js
+++ src/components/volunteer.js
@@ -1,42 +1,67 @@
-import React from "react";
-import { Button } from "@material-tailwind/react";
+import React from 'react';
+import { Button } from '@material-tailwind/react';
+import { motion, useAnimation } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
+
+const fadeInFromRight = {
+    hidden: { opacity: 0, x: 100 },
+    visible: { opacity: 1, x: 0, transition: { duration: 1 } }
+};
+
+const fadeInFromLeft = {
+    hidden: { opacity: 0, x: -100 },
+    visible: { opacity: 1, x: 0, transition: { duration: 1 } }
+};
 
 function VolunteerWithUs() {
-  return (
-    <div className="bg-gradient-to-r from-deep-orange-50 via-deep-orange-100 to-deep-orange-100/30 py-10 px-4 md:px-8 lg:px-8 xl:px-16 backdrop-filter: blur(5px) bg-white bg-opacity-70">
-      <div className="max-w-full w-11/12 lg:w-4/5 mx-auto flex flex-col md:flex-row-reverse justify-between items-center">
-        <div className="mb-8 md:mb-0 md:w-2/5 md:ml-8">
-          <h2 className="text-gray-900 text-3xl font-bold font-dela-gothic mb-4">
-            Volunteer With Us
-          </h2>
-          <p className="text-blue-gray-900 text-xl font-semibold mb-8 max-w-lg">
-            As a non-profit organization, we award volunteer hours to those who
-            contribute to open source projects. We also retain the ability to
-            distribute the President's Volunteer Service Award. Which is a
-            highly recognized award that is given to those who volunteer a
-            certain amount of hours. This looks great on college applications
-            and resumes.{" "}
-          </p>
-          <Button
-            size="lg"
-            ripple="light"
-            className="transition-all duration-300 ease-in-out shadow-md bg-calcommit-orange text-white font-dm-sans text-lg border-2 border-transparent capitalize hover:scale-105 hover:-rotate-6 hover:bg-white hover:border-calcommit-orange hover:text-black"
-            href="/volunteer"
-          >
-            Join Now
-          </Button>
-        </div>
+    const controlsText = useAnimation();
+    const controlsImage = useAnimation();
+    const [ref, inView] = useInView({ threshold: 0.1 });
 
-        <div className="transform hover:scale-105 transition-transform duration-300 md:w-2/5">
-          <img
-            src="polarbear.png"
-            alt="Volunteers Image"
-            className="rounded-md shadow-lg w-full h-auto"
-          />
+    React.useEffect(() => {
+        if (inView) {
+            controlsText.start('visible');
+            controlsImage.start('visible');
+        }
+    }, [controlsText, controlsImage, inView]);
+
+    return (
+        <div className="bg-gradient-to-r from-deep-orange-50 via-deep-orange-100 to-deep-orange-100/30 py-10 px-4 md:px-8 lg:px-8 xl:px-16 backdrop-filter: blur(5px) bg-white bg-opacity-70">
+            <div className="max-w-full w-11/12 lg:w-4/5 mx-auto flex flex-col md:flex-row-reverse justify-between items-center">
+
+                <motion.div 
+                    ref={ref}
+                    initial="hidden"
+                    animate={controlsText}
+                    variants={fadeInFromRight}
+                    className="mb-8 md:mb-0 md:w-2/5 md:ml-8"
+                >
+                    <h2 className="text-gray-900 text-3xl font-bold font-dela-gothic mb-4">Volunteer With Us</h2>
+                    <p className="text-blue-gray-900 text-xl font-semibold mb-8 max-w-lg">
+                        As a non-profit organization, we award volunteer hours to those who contribute to open source projects. We also retain the ability to distribute the President's Volunteer Service Award. Which is a highly recognized award that is given to those who volunteer a certain amount of hours. This looks great on college applications and resumes.
+                    </p>
+                    <Button 
+                        size="lg"
+                        ripple="light"
+                        className="transition-all duration-300 ease-in-out shadow-md bg-calcommit-orange text-white font-dm-sans text-lg border-2 border-transparent capitalize hover:scale-105 hover:-rotate-6 hover:bg-white hover:border-calcommit-orange hover:text-black"
+                        href="/volunteer"
+                    >
+                        Join Now
+                    </Button>
+                </motion.div>
+
+                <motion.div 
+                    className="md:w-2/5"
+                    initial="hidden"
+                    animate={controlsImage}
+                    variants={fadeInFromLeft}
+                >
+                    <img src="polarbear.png" alt="Volunteers Image" className="rounded-md shadow-lg w-full h-auto" />
+                </motion.div>
+
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default VolunteerWithUs;
