31bbd1728750d590d602c3677819d55a62342a0a
Aditya <adityasahas2025@gmail.com>
2023-08-27 23:30:40 -0700

    Revert "stupid build error"
    This reverts commit 539f62410884266f8b1b41e06a6fbdc9d8d7dbfa.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import CCFooter from "./components/Footer";
 import AboutPage from "./pages/AboutPage";
 import IndexPage from "./pages/IndexPage";
 
@@ -13,7 +13,7 @@ function App() {
         <Route path="/about" element={<AboutPage />} />
         <Route path="/" element={<IndexPage /> } />
       </Routes>
-      <Footer />
+      <CCFooter />
     </>
   );
 }
